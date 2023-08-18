b3e091a900c98349125ca989f95afa52a917ea40
zayan01 <zayan.shaikh@zikomosolutions.com>
2023-08-18 20:33:35 +0530

    updated modal ui

--- FlightBite.MVC/Utilities/UiUtils.cs
+++ FlightBite.MVC/Utilities/UiUtils.cs
@@ -2,11 +2,10 @@
 {
     public class UiUtils
     {
-
+        public bool isOpen = false; 
         public static string IsActive(string path ,string route)
         {
             return path.EndsWith(route) ? "active__menu" : "";
-
         }
     }
 }
