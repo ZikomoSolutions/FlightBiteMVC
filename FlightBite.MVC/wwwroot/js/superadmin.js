9f2d35302aeeb3180b34da528d2e3f190da7e23a
zayan01 <zayan.shaikh@zikomosolutions.com>
2023-08-18 11:28:40 +0530

    updated

--- FlightBite.MVC/wwwroot/js/superadmin.js
+++ FlightBite.MVC/wwwroot/js/superadmin.js
@@ -1,21 +1,41 @@
 let toggleModalClass = document.getElementById("toggle-modal");
-
-function addActiveClassOnModal() {
-    toggleModalClass.classList.add("modal-active");
-    document.body.style.overflow = "hidden";
-}
-function removeActiveClassOnModal() {
-    toggleModalClass.classList.remove("modal-active");
-    document.body.style.overflow = "auto";
-}
+let submitButtonOfModal = document.getElementById("submit-button");
+let crossIcon = document.getElementById("cross-icon");
+let openModalId = document.getElementById("open-modal");
+let modalContainer = document.getElementById("modal-container");
+openModalId.addEventListener("click", function () {
+    openModal();
+});
 
 
+toggleModalClass.addEventListener("click", function (e) {
+    if (e.target !== toggleModalClass) return;
+    closeModal();
+    console.log(e.target);
+});
 
+//document.addEventListener("click", function (e) {
+//    if (e.target != modalContainer) return;
+//    closeModal();
+//});
+
+submitButtonOfModal.addEventListener("click", function () {
+    closeModal();
+});
 
 document.addEventListener("keyup", function (e) {
     if (e.key == "Escape") {
-        toggleModalClass.classList.remove("modal-active");
-        document.body.style.overflow = "auto";
+        closeModal();
     }
 
 })
+
+
+function closeModal() {
+    toggleModalClass.classList.remove("modal-active");
+    document.body.style.overflow = "auto";
+}
+function openModal() {
+    toggleModalClass.classList.add("modal-active");
+    document.body.style.overflow = "hidden";
+}
